738f384b24fc4b18f77121e8fec4374b8b0b17c6
SiddheshKukade <littlelord124@gmail.com>
2022-08-12 09:14:43 +0530

    Add/UI/Images

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -8,11 +8,30 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(itemBuilder: (_ , index)),
-      // first parameter context and second parameter is index
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: 3,
+          itemBuilder: (_, index) {
+            // first parameter context and second parameter is index
+            //we want to scroll up and down
+            return Container(
+                width: double.maxFinite, // max width and height
+                height: double.maxFinite,
+                decoration: BoxDecoration(
+                  // decoration sets the background
+                  image: DecorationImage(
+                    image: AssetImage("img/" + images[index]),
+                  ),
+                ));
+          }),
     );
   }
 }
