70389f78d8d235d8a8a3885f3fe115123307a105
SiddheshKukade <littlelord124@gmail.com>
2022-08-12 08:51:56 +0530

    Add/Welcome-pagegit sg

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(itemBuilder: (_ , index)),
+      // first parameter context and second parameter is index
+    );
+  }
+}
