466d5a2f52ca4680589a3821c55caaebc5a64516
SiddheshKukade <littlelord124@gmail.com>
2022-08-17 19:43:27 +0530

    Chores/Add/Functions to fetch Data from server

--- lib/cubit/app_cubit.dart
+++ lib/cubit/app_cubit.dart
@@ -1,15 +1,33 @@
 import 'package:bloc/bloc.dart';
 import 'package:travel_app_flutter/cubit/app_cubit_states.dart';
+import 'package:travel_app_flutter/model/data_model.dart';
+import 'package:travel_app_flutter/services/data_services.dart';
 
 class AppCubits extends Cubit<CubitStates> {
+  final DataServices data;
+  late final places;
   // cubit is the emitter tht causes the state to change
-  AppCubits() : super(InitialState()) {
+  AppCubits(this.data) : super(InitialState()) {
     //1
     emit(WelcomeState()); //2 after 1 . 2 will be called automatically
   }
-  void getData() {
+
+  void getData() async {
     try {
       emit(LoadingState());
+      places = await data.getInfo();
+      print(await data.getInfo()); //getting info from the server
+      print("palces at app_cutibt");
+      print(places);
+      emit(LoadedState(places));
     } catch (e) {}
+  }
+
+  detailPage(DataModel data) {
+    emit(DetailState(data));
+  }
+
+  goHome() {
+    emit(LoadedState(places));
   }
 }
